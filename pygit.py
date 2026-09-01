8ab85200ab053325953edd252c92c02a799789d5
Ritik <gettoworitik12@gmail.com>
2026-09-01 07:29:52 +0530

    Refactor write_tree to accept directory argument

--- pygit.py
+++ pygit.py
@@ -42,11 +42,51 @@ def cat_file(sha1):
 
     sys.stdout.buffer.write(content)
 
-def write_tree():
+def write_tree(directory="."):
     tree_entries = []
 
-    # Iterate through the files in the current directory
-    for item in sorted(os.listdir():
+    #repeatdely scans the whole script to find all the files in the current directory and its subdirectories, and adds them to the tree_entries list
+    for item in sorted(os.listdir(directory)):
+        if item.startswith(".pygit") or item.startswith(".git"):
+            continue
+
+        full_path = os.path.join(directory, item)
+
+        if os.path.isfile(full_path):
+            #forming blob
+            sha1_hex = has_objects(full_path, write = True)
+            sha1_bytes = bytes.fromhex(sha1_hex)
+            
+            mode = "100644"
+
+            entry = f"{mode} {item}\0".encode("utf-8") + sha1_bytes
+            tree_entries.append(entry)
+
+
+        elif os.path.isdir(full_path):
+            sha1_hex = write_tree(full_path)
+            sha1_bytes = bytes.fromhex(sha1_hex)
+            mode = "40000"
+
+            entry = f"{mode} {item}\0".encode("utf-8") + sha1_bytes
+            tree_entries.append(entry)
+
+    #assembles the tree object by concatenating the entries and creating a header for the tree object, then compresses and writes it to the .pygit/objects directory
+    tree_content = b"".join(tree_entries)
+    header = f"tree {len(tree_content)}\0".encode("utf-8")
+    full_data = header + tree_content
+
+    sha_1 = hashlib.sha1(full_data).hexdigest()
+
+    dir_name = os.path.join(".pygit", "objects", sha_1[:2])
+    dir_file = os.path.join(dir_name, sha_1[2:])
+    os.makedirs(dir_name, exist_ok=True)
+
+    if not os.path.exists(dir_file):
+        with open(dir_file, "wb") as f:
+            f.write(zlib.compress(full_data))
+
+    return sha_1
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -63,3 +103,7 @@ if __name__ == "__main__":
     elif command == "cat-file":
         sha1 = sys.argv[2]
         cat_file(sha1)
+
+    elif command == "write-tree":
+        sha_1 = write_tree(".")
+        print(sha_1)
